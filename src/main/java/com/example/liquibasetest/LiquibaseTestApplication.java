6ff81e3870c1b19702a8f27c99e93631cc0469b0
Pedro Mantovani Antunes <pmantovani94@gmail.com>
2021-08-10 15:27:11 -0300

    Initial commit

---
+++ src/main/java/com/example/liquibasetest/LiquibaseTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseTestApplication.class, args);
+	}
+
+}
